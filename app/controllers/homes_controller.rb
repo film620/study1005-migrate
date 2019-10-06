dc91e5541387b5fd6d61fb4f6b83d66b267959f6
film620 <film620@daum.net>
2019-10-06 06:55:37 +0000

    되긴 했는데 왜 된건지??

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -5,20 +5,25 @@ end
 def second
     @age=params[:age].to_i 
     @name=params[:name]
-    
+   
     if @age < 20
         @age="미성년 가입 대상입니다.."     
     else
         @age="성인 가입대상입니다."
     end
+     alldata=Post.new  
+      alldata.age=params[:age]
+    alldata.name=params[:name]
+     alldata.save
+        @alldata=Post.all
     
     
 end
 
 def third
-     alldata=Post.new  
-    alldata.age = @age
-    
+    alldata=Post.new  
+    alldata.age=params[:age]
+    alldata.name=params[:name]
     alldata.save
     
    
